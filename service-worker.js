ac4e36762acf22d86ae046f5f63c800ace01bad3
GheetanEmpire <j3612681@gmail.com>
2025-04-09 18:08:34 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,31 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('static-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/about.html',
-        '/history.html',
-        '/medals.html',
-        '/news.html',
-        '/css/styles.css',
-        '/js/script.js',
-        'peHomO1P.png',
-        'peHomO1P-192x192.png',
-        'peHomO1P-512x512.png',
-        'offline.html',
-      ]);
+const cacheName = 'static-v1';
+const assets = [
+  '/',
+  'index.html',
+  'about.html',
+  'history.html',
+  'medals.html',
+  'news.html',
+  'styles.css',
+  'script.js',
+  'peHomO1P.png',
+  'peHomO1P-192x192.png',
+  'peHomO1P-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('Caching assets');
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      // If the file is cached, return it
-      return response || fetch(e.request).catch(() => {
-        // If fetch fails (e.g., no internet), serve the offline page
-        return caches.match('offline.html');
-      });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
